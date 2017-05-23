62a577e51029320f3b2ec53aecde1d26168237fa
Serafin <leaf466@gmail.com>
2017-05-23 12:24:40 -0700

    Adds layout of authentication

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const path = require('path')
 const db = require('./db/db')
 const app = express()
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+require('dotenv').config()
 
 const index = require('./routes/index')
 
 app.use('/', index)
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(session( { 
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
